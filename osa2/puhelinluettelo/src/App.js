89764d3955d0d41443b2643d497d0c3bb870e23f
Nikita Essine <nikita.essine@helsinki.fi>
2023-01-19 11:07:48 +0200

    2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import Filter from './components/Filter'
 import NewPersonForm from './components/NewPersonForm'
@@ -9,6 +10,18 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
 
   const addPerson = (event) => {
     event.preventDefault()
