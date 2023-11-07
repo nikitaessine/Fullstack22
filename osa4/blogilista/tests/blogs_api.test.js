f3c7f5e04b250e6dd0cf32268e1c80554ac7c0c2
Nikita Essine <nikita.essine@helsinki.fi>
2023-11-07 19:15:05 +0200

    4.9

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -4,12 +4,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('there are two notes', async () => {
+test('there are two blogs', async () => {
     const response = await api.get('/api/blogs')
   
     expect(response.body).toHaveLength(3)
   })
   
+
+test('all blogs have field called id', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body[0].id).toBeDefined()
+  })
+
 
 afterAll(async () => {
   await mongoose.connection.close()
