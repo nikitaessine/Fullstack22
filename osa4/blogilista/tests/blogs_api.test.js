580febfde6ae74469514e19bf803ad98baaba15b
Nikita Essine <nikita.essine@helsinki.fi>
2023-11-08 12:00:39 +0200

    4.11

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('there are two blogs', async () => {
+test('there are three blogs', async () => {
     const response = await api.get('/api/blogs')
   
     expect(response.body).toHaveLength(3)
@@ -33,7 +33,29 @@ test('a valid blog can be added and amount of blogs is increased', async () => {
       
     const blogsAtEnd = await api.get('/api/blogs')
     expect(blogsAtEnd.body).toHaveLength(4)
+
   })
+
+test('if likes is not defined, it is set to 0', async () => {
+    const newBlog = {
+        title: 'testi2',
+        author: 'testaaja2',
+        url: 'testi2.fi'
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-type', /application\/json/)
+      
+    const blogsAtEnd = await api.get('/api/blogs')
+
+    const nolikes = blogsAtEnd.body.find(r => r.author === "testaaja2")
+
+    expect(nolikes.likes).toBe(0)
+  })
+
 
 afterAll(async () => {
   await mongoose.connection.close()
