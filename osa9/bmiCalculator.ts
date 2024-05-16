7a24d920ee960941a8b350cb1adf4d11a5dc2d1a
nikitaessine <nikita@MacBook-Pro.local>
2024-05-16 16:41:12 +0300

    9.5

--- osa9/bmiCalculator.ts
+++ osa9/bmiCalculator.ts
@@ -1,23 +1,6 @@
-interface BmiValues {
-    height: number;
-    weight: number;
-}
 
-const parseArguments = (args: number[]): BmiValues => {
-    if (args.length < 2) throw new Error('Not enough arguments');
-    if (args.length > 2) throw new Error('Too many arguments');
 
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            height: Number(args[2]),
-            weight: Number(args[3])
-        };
-    } else {
-        throw new Error('Provided values were not numbers!');
-    }
-}
-
-function calculateBmi(height: number, weight: number): string {
+export function calculateBmi(height: number, weight: number): string {
     const bmi = weight / Math.pow(height / 100, 2);
     if (bmi < 18.5) {
         return 'Underweight';
@@ -28,28 +11,4 @@ function calculateBmi(height: number, weight: number): string {
     } else {
         return 'Obese';
     }
-}
-
-try {
-    const args = process.argv.slice(2).map(arg => {
-        const number = Number(arg);
-        if (isNaN(number)) {
-            throw new Error('Please provide numbers only');
-        }
-        return number;
-    }); 
-
-    if (args.length !== 2) {
-        throw new Error('Please provide two arguments - height and weight');
-    }
-
-    const height = args[0];
-    const weight = args[1];
-    console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-    let errorMessage = 'Error: ';
-    if (error instanceof Error) {
-        errorMessage += error.message; 
-    }
-    console.log(errorMessage);
 }
--- osa9/index.ts
+++ osa9/index.ts
@@ -1,10 +1,22 @@
 import express = require('express');
 const app = express();
+import { calculateBmi } from './bmiCalculator';
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    if (isNaN(height) || isNaN(weight)) {
+        res.status(400).send({ error: "malformatted parameters" });
+    } else {
+        const bmi = calculateBmi(height, weight);
+        res.send({
+            weight,
+            height,
+            bmi
+        });
+    }
 });
-
 const PORT = 3003;
 
 app.listen(PORT, () => {
